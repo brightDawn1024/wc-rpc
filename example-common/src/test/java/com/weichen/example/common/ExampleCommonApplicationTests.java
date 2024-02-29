48718390c2501fb72c82a3c0b715fc47c6622189
sun <sun@weichen.com>
2024-02-29 21:32:11 +0800

    构建基础的RPC框架

---
+++ example-common/src/test/java/com/weichen/example/common/ExampleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weichen.example.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ example-provider/src/main/java/com/weichen/example/provider/ExampleProviderApplication.java
@@ -0,0 +1,13 @@
+package com.weichen.example.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleProviderApplication.class, args);
+    }
+
+}
