17303c92ebb769dd878adb2a9623d14fb0e29f00
sun <sun@weichen.com>
2024-03-01 22:19:26 +0800

    构建基础的4个模块，编写公共模块中的数据类型和接口

---
+++ example-common/src/main/java/com/weichen/example/common/service/UserService.java
@@ -0,0 +1,17 @@
+package com.weichen.example.common.service;
+
+import com.weichen.example.common.model.User;
+
+/**
+ *  用户服务
+ */
+public interface UserService {
+
+    /**
+     *  获取用户
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+
+}
