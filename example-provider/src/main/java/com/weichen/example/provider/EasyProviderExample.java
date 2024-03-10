48c8750f4d8bd8151afde5720c0f9ea2b8a54a69
sun <sun@weichen.com>
2024-03-10 22:54:53 +0800

    添加 web服务器 Vert.x

--- example-provider/src/main/java/com/weichen/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/weichen/example/provider/EasyProviderExample.java
@@ -1,12 +1,16 @@
 package com.weichen.example.provider;
 
+import com.weichen.wcrpc.server.VertxHttpServer;
+
 /**
  *  简单服务提供者示例
  */
 public class EasyProviderExample {
 
     public static void main(String[] args) {
-        // 提供服务 todo
+        // 启动 web 服务
+        VertxHttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 
 }
