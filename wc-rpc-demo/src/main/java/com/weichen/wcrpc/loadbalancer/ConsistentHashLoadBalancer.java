3d1a01a37039cd315233ec913fdef6424e8302f1
sun <sun@weichen.com>
2024-04-10 11:57:28 +0800

    实现3种负载均衡器

---
+++ wc-rpc-demo/src/main/java/com/weichen/wcrpc/loadbalancer/ConsistentHashLoadBalancer.java
@@ -0,0 +1,61 @@
+package com.weichen.wcrpc.loadbalancer;
+
+import com.weichen.wcrpc.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *  一致性哈希负载均衡器
+ */
+public class ConsistentHashLoadBalancer implements LoadBalancer{
+
+    /**
+     *  一致性 Hash 环，存放虚拟节点
+     */
+    private final TreeMap<Integer, ServiceMetaInfo> virtualNodes = new TreeMap<>();
+
+    /**
+     *  虚拟节点数
+     */
+    private static final int VIRTUAL_NODE_NUE = 100;
+
+    @Override
+    public ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (serviceMetaInfoList.isEmpty()){
+            return null;
+        }
+
+        // 构建虚拟节点环
+        for (ServiceMetaInfo serviceMetaInfo : serviceMetaInfoList){
+            for (int i = 0;i < VIRTUAL_NODE_NUE;i++){
+                int hash = getHash(serviceMetaInfo.getServiceAddress() + "#" + i);
+                virtualNodes.put(hash,serviceMetaInfo);
+            }
+        }
+
+        // 获取调用请求的 hash 值
+        int hash = getHash(requestParams);
+
+        // 选择最接近且大于等于调用请求 hash 值的虚拟节点
+        Map.Entry<Integer, ServiceMetaInfo> entry = virtualNodes.ceilingEntry(hash);
+
+        // 没有大于等于调用请求 hash 值的虚拟节点
+        if (entry == null){
+            // 返回环首部的节点
+            entry = virtualNodes.firstEntry();
+        }
+
+        return entry.getValue();
+    }
+
+    /**
+     *  简单的 Hash 算法，但在实际的应用中，需要根据具体的需求和场景实现更复杂的哈希算法，以确保良好的负载均衡和性能。
+     * @param key
+     * @return
+     */
+    private int getHash(Object key){
+        return key.hashCode();
+    }
+}
